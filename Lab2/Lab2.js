e729506fc92de2e3946e20554eca58b51f2fa636
Serg7let <sergijba@gmail.com>
2021-11-16 21:25:49 +0200

    аауууааау

--- Lab2/Lab2.js
+++ Lab2/Lab2.js
@@ -18,3 +18,60 @@ function getAllworkers() {
 function getWorkersByID(id) {
     return getAllworkers().find(function (e) { return e.id == id; });
 }
+function printWorker(worker) {
+    console.log(worker.name + " " + worker.surname + " got salary " + worker.salary);
+}
+function LPR(text) {
+    console.log("text is " + text);
+}
+var logPrize = LPR;
+logPrize("hey");
+var favouriteAuthor = { name: "Bonka", email: "Donkas@gmail.ua", numBooksPublished: 15 };
+//let favouriteLibrarian: Librarian = {
+//    name: `Tom`, email: `TomHanks@yahoo.com`, department: `Cringe`, assistCustomer: function func(cust: string) {
+//        console.log(cust);
+//    }
+//}
+var UniversityLibrarian = /** @class */ (function () {
+    function UniversityLibrarian() {
+    }
+    UniversityLibrarian.prototype.assistCustomer = function (custName) {
+        console.log(this.name + " is assisting " + custName);
+    };
+    return UniversityLibrarian;
+}());
+var favouriteLibrarian = new UniversityLibrarian();
+favouriteLibrarian.name = "John";
+favouriteLibrarian.assistCustomer("Dohn");
+var ReferenceItem = /** @class */ (function () {
+    /*title: string
+    year: number
+    constructor(newTitle: string, newYear: number) {
+        console.log(`Creating a new ReferenceItem.....................`);
+        this.title = newTitle
+        this.year = newYear
+    }*/
+    function ReferenceItem(title, year) {
+        this.title = title;
+        this.year = year;
+        this.department = "Cringe";
+    }
+    Object.defineProperty(ReferenceItem.prototype, "publisher", {
+        get: function () {
+            return this._publisher.toUpperCase();
+        },
+        set: function (newPublisher) {
+            this._publisher = newPublisher;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    ReferenceItem.prototype.printItem = function () {
+        console.log(this.title + " was published in " + this.year + " by " + this.department + " department");
+    };
+    return ReferenceItem;
+}());
+var ref = new ReferenceItem("Harry Potter", 1942);
+ref.printItem();
+ref.publisher = "Ron Whisley";
+console.log(ref.publisher);
--- Lab2/Lab2.ts
+++ Lab2/Lab2.ts
@@ -13,6 +13,25 @@ interface Worker {
     available: boolean
     salary: number
     category: Category
+    markPrize: PrizeLogger
+}
+
+interface PrizeLogger {
+    (text: string): void
+}
+
+interface Person {
+    name: string
+    email: string
+}
+
+interface Author extends Person {
+    numBooksPublished: number
+}
+
+interface Librarian extends Person {
+    department: string
+    assistCustomer(custName: string): void
 }
 
 function getAllworkers() {
@@ -28,3 +47,66 @@ function getAllworkers() {
 function getWorkersByID(id: number): Worker {
     return getAllworkers().find(e => e.id == id)
 }
+
+function printWorker(worker: Worker) {
+    console.log(worker.name + ` ` + worker.surname + ` got salary ` + worker.salary)
+}
+
+function LPR(text: string) {
+    console.log(`text is ${text}`)
+}
+
+let logPrize: PrizeLogger = LPR
+logPrize(`hey`)
+
+let favouriteAuthor: Author = { name: `Bonka`, email: `Donkas@gmail.ua`, numBooksPublished: 15 }
+//let favouriteLibrarian: Librarian = {
+//    name: `Tom`, email: `TomHanks@yahoo.com`, department: `Cringe`, assistCustomer: function func(cust: string) {
+//        console.log(cust);
+//    }
+//}
+
+class UniversityLibrarian implements Librarian {
+    name: string;
+    email: string;
+    department: string;
+    assistCustomer(custName: string): void {
+        console.log(`${this.name} is assisting ${custName}`);
+    }
+}
+
+let favouriteLibrarian: Librarian = new UniversityLibrarian();
+favouriteLibrarian.name = "John"
+favouriteLibrarian.assistCustomer("Dohn")
+
+class ReferenceItem {
+    /*title: string
+    year: number
+    constructor(newTitle: string, newYear: number) {
+        console.log(`Creating a new ReferenceItem.....................`);
+        this.title = newTitle
+        this.year = newYear
+    }*/
+
+    constructor(public title: string, protected year: number) { }
+
+    private _publisher: string;
+    department: string = `Cringe`
+
+    public get publisher(): string {
+        return this._publisher.toUpperCase();
+    }
+    public set publisher(newPublisher: string) {
+        this._publisher = newPublisher;
+    }
+
+    printItem(): void {
+        console.log(`${this.title} was published in ${this.year} by ${this.department} department`);
+    }
+}
+
+let ref: ReferenceItem = new ReferenceItem(`Harry Potter`, 1942)
+ref.printItem()
+
+ref.publisher = `Ron Whisley`
+console.log(ref.publisher);
