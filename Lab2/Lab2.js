8068f9a215fa160e4d0fc800755e940f8dd28401
Serg7let <sergijba@gmail.com>
2021-11-16 19:01:09 +0200

    anime backwards is emina(m)

--- Lab2/Lab2.js
+++ Lab2/Lab2.js
@@ -1,1 +1,20 @@
-var MeDie;
+var Category;
+(function (Category) {
+    Category["BA"] = "Business Analyst";
+    Category["Dev"] = "Developer";
+    Category["Des"] = "Designer";
+    Category["QA"] = "Quality Assurance";
+    Category["SM"] = "Scrum Master";
+})(Category || (Category = {}));
+function getAllworkers() {
+    var workers = [
+        { name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000, Category: Category.SM, id: 1 },
+        { name: 'Petro', surname: 'Petrov', available: true, salary: 1500, Category: Category.Des, id: 2 },
+        { name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600, Category: Category.BA, id: 3 },
+        { name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300, Category: Category.QA, id: 4 }
+    ];
+    return workers;
+}
+function getWorkersByID(id) {
+    return getAllworkers().find(function (e) { return e.id == id; });
+}
--- Lab2/Lab2.ts
+++ Lab2/Lab2.ts
@@ -1,1 +1,30 @@
-let MeDie
+enum Category {
+    BA = "Business Analyst",
+    Dev = "Developer",
+    Des = "Designer",
+    QA = "Quality Assurance",
+    SM = "Scrum Master"
+}
+
+interface Worker {
+    id: number
+    name: string
+    surname: string
+    available: boolean
+    salary: number
+    category: Category
+}
+
+function getAllworkers() {
+    let workers = [
+        { name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000, Category: Category.SM, id: 1 },
+        { name: 'Petro', surname: 'Petrov', available: true, salary: 1500, Category: Category.Des, id: 2 },
+        { name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600, Category: Category.BA, id: 3 },
+        { name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300, Category: Category.QA, id: 4 }
+    ]
+    return workers;
+}
+
+function getWorkersByID(id: number): Worker {
+    return getAllworkers().find(e => e.id == id)
+}
